2343465b27332f3b65bdad61b103bc1a8e5495ec
andre-diass <adsf@poli.br>
2023-08-04 22:47:54 -0300

    fix: revert previous changes

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,10 +1,30 @@
 import { signOut, useSession, getSession } from "next-auth/react";
 
 export default function App() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <>
       <button onClick={() => signOut()}>Sign out</button>
       <p> this is the app protected page</p>
     </>
   );
+}
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      data: session.user,
+    },
+  };
 }
