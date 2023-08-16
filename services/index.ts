d1bc20947a28f551b12a6c05234ef7cdc1020263
andre-diass <adsf@poli.br>
2023-08-16 10:14:05 -0300

    feat: secure API

--- services/index.ts
+++ services/index.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "https://ske84d6xyj.execute-api.us-west-1.amazonaws.com/dev/serverlessSetup/",
+  baseURL: process.env.STORE_ADMIN_API_BASE_URL as string,
 });
 
 export default api;
