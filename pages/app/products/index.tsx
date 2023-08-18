be90d593b08123baa1c6418f7d561cb11b38438a
andre-diass <adsf@poli.br>
2023-08-18 16:27:12 -0300

    feat: implement getRequest initial implementation

--- pages/app/products/index.tsx
+++ pages/app/products/index.tsx
@@ -1,17 +1,30 @@
 import Link from "next/link";
 import { protectedRouteMiddleware } from "../../../middlewares/protectedRouteMiddleware";
+import axios from "axios";
 
 export default function Products() {
+  async function makeGetRequest() {
+    const response = await axios.get(
+      "https://ske84d6xyj.execute-api.us-west-1.amazonaws.com/dev/serverlessSetup/getProduct"
+    );
+    console.log(response.data);
+  }
+
   return (
     <>
       <Link
         className="bg-slate-100 rounded-lg p-2 text-zinc-900"
         href={"/app/products/new"}
       >
-        {" "}
-        New product{" "}
+        New product
       </Link>
       <p className="mt-4">this is the app products page</p>
+      <button
+        onClick={makeGetRequest}
+        className="bg-white text-black p-2 rounded-lg mt-5"
+      >
+        Make get request
+      </button>
     </>
   );
 }
