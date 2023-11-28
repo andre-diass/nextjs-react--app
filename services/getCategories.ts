e8b7dff25b984c3d557d42cdb8f29df31a774923
ANDRÉ DIAS S <adsf@poli.br>
2023-11-28 11:14:23 -0300

    test: deploy on vercel with new baseURL for the api

--- services/getCategories.ts
+++ services/getCategories.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function getCategories(userId: string) {
   try {
     const response = await axios.get(
-      "https://ske84d6xyj.execute-api.us-west-1.amazonaws.com/dev/serverless/getCategories/" +
+      "https://vclqy6j2ed.execute-api.us-west-1.amazonaws.com/dev/serverless/getCategories/" +
         userId
     );
     return response.data;
