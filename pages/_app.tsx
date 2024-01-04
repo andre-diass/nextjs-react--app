dceb2df0ba0b9460adf88c941e028c1c3ad1186b
ANDRÉ DIAS S <adsf@poli.br>
2024-01-04 16:30:07 -0300

    feat: add npprogress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import { SessionProvider } from "next-auth/react";
 import { CustomAppProps } from "../types/nextApp";
 import ProtectedLayout from "@/components/layouts/ProtectedLayout";
 import PublicLayout from "@/components/layouts/PublicLayout";
+import NProgress from "nprogress";
+import Router from "next/router";
+
+NProgress.configure({ showSpinner: false, trickle: true });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({
   Component,
