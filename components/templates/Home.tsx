08defd0bd3c0f4e13c4daf7c261f37d33e5e84fd
andre-diass <adsf@poli.br>
2024-11-29 23:08:06 -0300

    fix: add image to inde

--- components/templates/Home.tsx
+++ components/templates/Home.tsx
@@ -1,17 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
 import { signIn } from "next-auth/react";
 
 const Home = () => {
   return (
     <>
+      <img
+        className="m-5 box-content "
+        src="/cropped-headerteste-2.png"
+        alt="teste"
+      />
       <div className="text-center">
         <button
           onClick={() => signIn("google")}
           className="bg-white mb-5 dark:text-slate-950 text-black rounded-lg p-2 px-4"
         >
-          {" "}
-          Google log in{" "}
+          Google log in
         </button>
-        <p> Welcome to the login page!</p>
+        <p>
+          {" "}
+          Projeto Sistema de rastreamento de ativos utilizando tecnologia LTE-M{" "}
+        </p>
       </div>
     </>
   );
