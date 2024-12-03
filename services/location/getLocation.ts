0770d10d8f82c440363d204581a1d1a4f781cf56
andre-diass <adsf@poli.br>
2024-12-03 13:46:45 -0300

    feat: mock date

--- services/location/getLocation.ts
+++ services/location/getLocation.ts
@@ -3,7 +3,7 @@ import api, { api_monolith } from "../index";
 export async function getLocation() {
   try {
     const response = await api_monolith.get("/location", {
-      params: { imei: "869951036930547", reference_date: "12/02/2024" },
+      params: { imei: "869951036930547", reference_date: "12/03/2024" },
     });
 
     return response.data;
